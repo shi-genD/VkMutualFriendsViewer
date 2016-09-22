2f097ccbf60112adb126f7b4c05ec463db7c6b16
shi <malishevsky.al@gmail.com>
2016-09-22 15:39:27 +0300

    added invalid input handling, avatars's load realized in own thread

--- src/main/java/main/controllers/MainOverviewController.java
+++ src/main/java/main/controllers/MainOverviewController.java
@@ -7,18 +7,25 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import main.Main;
 import main.apivk.APIvk;
 import main.apivk.VkUser;
-import org.apache.http.HttpException;
+import main.net.ImageLoaderCallable;
+import main.net.ImageViewAsyncApplier;
+import main.utils.UserNotFoundException;
 import org.json.JSONException;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import static java.util.stream.Collectors.toList;
 
 public class MainOverviewController {
 
@@ -29,11 +36,18 @@ public class MainOverviewController {
     private TextField secondId;
 
     @FXML
+    private Label firstErrMsg;
+
+    @FXML
+    private Label secondErrMsg;
+
+    @FXML
     private Button btnGo;
 
     @FXML
     private FlowPane flowPane;
 
+    private Set<VkUser> mutualFriends;
     private Main mainApp;
 
     @FXML
@@ -41,26 +55,45 @@ public class MainOverviewController {
         flowPane.setPadding(new Insets(5, 5, 5, 5));
         flowPane.setVgap(5);
         flowPane.setHgap(5);
-        flowPane.setAlignment(Pos.CENTER);
+        flowPane.setAlignment(Pos.TOP_CENTER);
     }
 
     @FXML
-    private void handleGo() {
-        String id1 = firstId.getText();
-        String id2 = secondId.getText();
-        if (id1.length()!=0 && id2.length()!=0) {
-            showResult(id1, id2);
+    private void handleGo() throws InterruptedException {
+        flowPane.getChildren().clear();
+        firstErrMsg.setText("");
+        secondErrMsg.setText("");
+
+        APIvk apiVk = new APIvk();
+        try {
+            String id1 = apiVk.parseInputId(firstId.getText(), 1);
+            String id2 = apiVk.parseInputId(secondId.getText(), 2);
+            mutualFriends = apiVk.getMutualFriends(id1, id2);
+            showResult(mutualFriends);
+        } catch (UserNotFoundException e) {
+            //e.printStackTrace();
+            if (e.getMessage().startsWith("1"))
+                firstErrMsg.setText(e.getMessage().substring(2));
+            else if (e.getMessage().startsWith("2"))
+                secondErrMsg.setText(e.getMessage().substring(2));
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
         }
     }
 
-    private void showResult(String id1, String id2){
+    private void showResult(Set<VkUser> mutualFriends){
 
-        APIvk apiVk = new APIvk();
-        Set<VkUser> mutualFriends;
-        try {
-            mutualFriends = apiVk.getMutualFriends(id1, id2);
+            if (mutualFriends.size()==0) {
+                flowPane.getChildren().add(new Label("Mutual friends not found"));
+                return;
+            }
+            Map<VkUser, Future<Image>> friendsPhotos = getUserPhotos(mutualFriends);
+            ImageViewAsyncApplier imageViewApplier = new ImageViewAsyncApplier();
+
             for (VkUser user : mutualFriends) {
-                ImageView avatar = new ImageView(user.getPhotoURL());
+                ImageView avatar = new ImageView();
+                Future<Image> imageFuture = friendsPhotos.get(user);
+                imageViewApplier.add(imageFuture, avatar);
                 String userUrl = "http://vk.com/id" + user.getUserId();
                 Button userView = new Button(user.getFirstName()+"\n"
                         + user.getLastName(), avatar);
@@ -69,12 +102,31 @@ public class MainOverviewController {
                 userView.setContentDisplay(ContentDisplay.TOP);
                 flowPane.getChildren().add(userView);
             }
-
-        } catch (IOException | URISyntaxException | HttpException | JSONException e) {
-            e.printStackTrace();
-        }
-
+            imageViewApplier.startApplying();
     }
 
     public void setMain(Main mainApp) { this.mainApp = mainApp; }
+
+    private static Map<VkUser, Future<Image>> getUserPhotos(Collection<VkUser> users) {
+        final ExecutorService executorService = Executors.newCachedThreadPool();
+
+        List<Future<Image>> imageFutures = users.stream()
+                .map(VkUser::getPhotoURL)
+                .map(ImageLoaderCallable::new)
+                .map(executorService::submit)
+                .collect(toList());
+
+        Map<VkUser, Future<Image>> result = new HashMap<>();
+
+        int index = 0;
+        Iterator<VkUser> userIterator = users.iterator();
+
+        while (userIterator.hasNext()) {
+
+            result.put(userIterator.next(),
+                    imageFutures.get(index++));
+
+        }
+        return result;
+    }
 }
